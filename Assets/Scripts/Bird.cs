0362b8ab377eafee4c33129496c69d98df60da7c
linyuhe <yuhe.lin@foxmail.com>
2020-05-18 21:08:01 +0800

    bird脚本

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	private void OnMouseDrag()
+	{
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		// 调整z轴坐标=0
+		transform.position -= new Vector3(0, 0, Camera.main.transform.position.z);
+	}
+}
