a26feaaddbc9f02723feed9b71617547beeb9cb9
linyuhe <yuhe.lin@foxmail.com>
2020-07-25 11:08:05 +0800

    实现游戏的暂停和继续

---
+++ Assets/Scripts/MainUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour
+{
+
+    public Button PauseBtn;
+
+    public PausePanel PausePanel;
+
+
+    private void Start()
+    {
+        PauseBtn.onClick.AddListener(ShowPausePanel);
+    }
+
+
+    private void ShowPausePanel()
+    {
+        PauseBtn.gameObject.SetActive(false);
+        PausePanel.Show();
+    }
+
+    public void ShowPauseBtn()
+    {
+        PauseBtn.gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/PausePanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PausePanel : MonoBehaviour
+{
+
+	private Animator _animator;
+
+	public Button ResumeBtn;
+
+	private Image _background;
+
+	public MainUI MainUI;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		_background = GetComponent<Image>();
+		_animator = GetComponent<Animator>();
+		ResumeBtn.onClick.AddListener(Hide);
+	}
+
+	public void Show()
+	{
+		_background.enabled = true;
+		_animator.SetBool("Show", true);
+	}
+
+	public void Hide()
+	{
+		_animator.SetBool("Show", false);
+	}
+
+	public void OnHideEnd()
+	{
+		_background.enabled = false;
+		MainUI.ShowPauseBtn();
+	}
+}
